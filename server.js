156a255015bd6be0c4da86032eddd3d0c112c494
Albert Zhang <aalbertzhang@gmail.com>
2021-03-06 17:51:48 -0800

    add session storage

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
 const app = express();
 const lessonRouter = require('./routers/lesson.router');
 const config = require('./config');
@@ -11,12 +13,27 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
+app.use(
+  session({
+    secret: 'secrettaefrjah',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // make sure app is running
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  session.test = 'test';
+  console.log(session.test);
 });
 
 app.use('/lessons', lessonRouter);
