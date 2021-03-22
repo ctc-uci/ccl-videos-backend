094f53a5bedb1e84b2eed8f2e14a0e20b6d89444
Albert Zhang <aalbertzhang@gmail.com>
2021-03-22 21:56:36 +0000

    Add lessonId to lesson schema

--- models/lesson.schema.js
+++ models/lesson.schema.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const lessonSchema = new mongoose.Schema({
+  lessonId: String,
   title: String,
   description: String,
   videoUrl: String,
+  thumbnailUrl: String,
   codes: [
     {
       code: String,
