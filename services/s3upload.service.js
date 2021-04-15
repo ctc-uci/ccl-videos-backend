986e2f4c674ba865d96c7a0f962090f49ef12285
Albert Zhang <aalbertzhang@gmail.com>
2021-04-14 22:36:26 -0700

    modify s3 uploader

--- services/s3upload.service.js
+++ services/s3upload.service.js
@@ -1,17 +1,43 @@
-const AWS = require("aws-sdk");
-const config = require("../config");
+const aws = require("aws-sdk");
+const { v4: uuid } = require("uuid");
+
+const bucketMap = {
+  "ccl-videos": "video",
+};
 
 module.exports = {
-  getUploadUrl: async () => {
-    AWS.config.update(config.awsConfig);
-    const s3 = new AWS.S3();
-    var params = {
-      ACL: "public-read",
-      Bucket: 'ccl-videos',
-      Key: 'test3.mp4',
-      ContentType: 'video/mp4'
+  getUploadUrl: async (getSignedURLDTO) => {
+    const { contentType, bucket } = getSignedURLDTO;
+    const fileExtension = contentType ? contentType.split(/\//)[1] : null;
+
+    if (!fileExtension) {
+      throw new Error(`Recieved unexpected content-type: ${fileExtension}`);
     }
-    const url = s3.getSignedUrl('putObject', params);
-    return url;
+
+    if (!bucketMap[bucket]) {
+      throw new Error(`Include bucket, inputed: ${bucket}, expected ${Object.keys(bucketMap).join(",")}`);
+    }
+
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: process.env.ACCESSKEYID,
+      secretAccessKey: process.env.SECRETACCESSKEY,
+      region: process.env.REGION,
+    });
+
+    const s3 = new aws.S3();
+    const URL_EXPIRATION_SECONDS = 300;
+    const fileExtention = `${bucketMap[bucket]}/${uuid()}.${fileExtension}`;
+
+    const s3Params = {
+      Bucket: process.env.S3_BUCKET,
+      Key: fileExtention,
+      Expires: URL_EXPIRATION_SECONDS,
+      ContentType: `${contentType}`,
+      ACL: "public-read",
+    };
+
+    const uploadURL = s3.getSignedUrl("putObject", s3Params);
+    return { uploadURL, key: fileExtention };
   },
 }
